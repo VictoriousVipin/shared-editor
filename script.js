b252e2670f901ff9b14ab59a25271d264c645c2d
Vipin Pal Tomar <viptomar@publicisgroupe.net>
2021-01-20 22:22:42 +0530

    sds

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const area = document.getElementById("shared-editor-textarea");
 area.addEventListener("change", (e) => {
   socket.emit("content-add", e.target.value);
 });
+area.addEventListener("keyup", (e) => {
+  socket.emit("content-add", e.target.value);
+});
 
 socket.on("content-update", (content) => {
   area.value = content;
